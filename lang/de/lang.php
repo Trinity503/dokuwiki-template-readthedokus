09814a2a973294fcc30926965259dce9efc50478
erhardma <54348970+erhardma@users.noreply.github.com>
2023-05-23 19:28:22 +0200

    first changes

---
+++ lang/de/lang.php
@@ -0,0 +1,55 @@
+<?php
+
+// style.ini values
+
+$lang['__link__'] = 'Link Farbe';
+$lang['__missing__'] = 'Link color (missing)';
+$lang['__text_theme__'] = 'Text color on theme color';
+$lang['__background_theme__'] = 'Main theme color';
+$lang['__text_sidebar__'] = 'Sidebar Textfarbe';
+$lang['__background_sidebar__'] = 'Sidebar Hintergrundfarbe';
+$lang['__background_sidebar_hover__'] = 'Sidebar background highlight color (on hover)';
+$lang['__text_sidebar_caption__'] = 'Sidebar caption color';
+$lang['__text_sidebar_toc__'] = 'Sidebar toc text color';
+$lang['__background_sidebar_toc__'] = 'Sidebar toc background color';
+$lang['__background_sidebar_toc_hover__'] = 'Sidebar toc background highlight color (on hover)';
+$lang['__background_sidebar_toc_highlight__'] = 'Sidebar toc background highlight color';
+$lang['__text_sidebar_expand1__'] = 'Sidebar toc expand button text color';
+$lang['__text_sidebar_expand2__'] = 'Sidebar toc expand button text color for > level2';
+$lang['__text_pagefooter__'] = 'Page footer text color';
+$lang['__text_monospace__'] = 'Mono space text color';
+$lang['__background_monospace__'] = 'Mono space background color';
+$lang['__border_monospace__'] = 'Mono space border color';
+$lang['__background_code__'] = 'Code Hintergrundfarbe';
+$lang['__border_code__'] = 'Code Rahmenfarbe';
+$lang['__text_button__'] = 'Button text color';
+$lang['__background_button__'] = 'Button Hintergrundfarbe';
+$lang['__text_button_hover__'] = 'Button Textfarbe (on hover)';
+$lang['__background_button_hover__'] = 'Button Hintergrundfarbe (on hover)';
+$lang['__border_edit_highlight__'] = 'Edit highlight border color';
+$lang['__background_edit_highlight1__'] = 'Edit highlight Hintergrundfarbe1';
+$lang['__background_edit_highlight2__'] = 'Edit highlight Hintergrundfarbe2';
+$lang['__width_site__'] = 'Site width (only used when centering)';
+$lang['__width_content__'] = 'Content width';
+$lang['__width_sidebar__'] = 'Side bar width';
+$lang['__background_header__'] = 'Header Hintergrundfarbe';
+$lang['__background_footer__'] = 'Footer Hintergrundfarbe';
+$lang['__height_header__'] = 'Header Höhe';
+$lang['__height_footer__'] = 'Footer Höhe';
+$lang['__media_query_sp__'] = 'CSS breakpoint for smart phones';
+$lang['__media_query_tb__'] = 'CSS breakpoint for tablets';
+
+$lang['breadcrumbs_docs'] = 'Docs';
+$lang['breadcrumbs_admin'] = 'Admin';
+$lang['pageheader_editthispage'] = 'Seite bearbeiten';
+$lang['btn_next'] = 'Weiter';
+$lang['btn_previous'] = 'Zur&uuml;ck';
+$lang['toc_title'] = 'Inhalt';
+$lang['js']['searchform_placeholder'] = 'Suche';
+
+// Admonition
+
+$lang['admonition_note'] = 'Note';
+$lang['admonition_tip'] = 'Tip';
+$lang['admonition_danger'] = 'Danger';
+$lang['admonition_caution'] = 'Caution';
